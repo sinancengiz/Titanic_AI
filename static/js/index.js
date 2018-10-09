ef650ee7740f4766f74d2f288a3ac7a3ec24f82e
Sinan Cengiz <sinancengiz_84@hotmail.com>
2018-10-09 12:01:48 -0700

    new graph added to result html

--- static/js/index.js
+++ static/js/index.js
@@ -53,3 +53,15 @@ Plotly.d3.json(pasclass_url, function(error, response) {
     }
     Plotly.plot("bar4", data, layout)
 })
+
+Plotly.d3.json(embarked_url, function(error, response) {
+    if (error) return console.warn(error);
+    var data = [response];
+    console.log(response)
+    var layout = { margin: { t: 30, b:50 },
+                   title: "Number of New User by Ports",
+                   xaxis: { title: "User Embarked from the Ports "},
+                   yaxis: { title: "Number of New User"}            
+    }
+    Plotly.plot("bar5", data, layout)
+})
